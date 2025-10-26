8862f63603ed5e9ff4ba6d1650819079b5d2bf94
Lee Yunjin <gzblues61@daum.net>
2025-10-26 12:24:52 +0900

    remove pkexec

--- replace_script_to_bin.sh
+++ replace_script_to_bin.sh
@@ -4,4 +4,4 @@ python3 -m nuitka --standalone --onefile yoonzip.py\
   --include-package=pyzipper
 rm -rf /usr/bin/yoonzip
 rm -rf /usr/bin/yoonzip.py
-pkexec cp yoonzip.bin /usr/bin/yoonzip
+cp yoonzip.bin /usr/bin/yoonzip
